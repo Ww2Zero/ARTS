1011899817cf4e70507c0e49228f93d0ecebda5e
wuaq <wuaq@szkingdom.com>
2020-06-03 19:52:20 +0800

    feat(Code4Offer): 062二叉树的最近公共祖先

---
+++ Algorithm/Code4Offer/062/solution.h
@@ -0,0 +1,32 @@
+// https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (root == NULL || root == p || root == q) {
+            return root;
+        }
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+        if (left == NULL && right == NULL) {
+            return NULL;
+        }
+        if (left == NULL) {
+            return right;
+        }
+
+        if (right == NULL) {
+            return left;
+        }
+        return root;
+    }
+};
